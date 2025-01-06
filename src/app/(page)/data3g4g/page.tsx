ba94dd9a1257085b486f176479c6ed0544328ee2
Pham The Phong <emyeugay12345@gmail.com>
2025-01-06 19:46:36 +0700

    fix loi

--- src/app/(page)/data3g4g/page.tsx
+++ src/app/(page)/data3g4g/page.tsx
@@ -17,15 +17,15 @@ export default function Data3g() {
         </div>
         <ul className="font-normal text-18">
           <li>
-            (*) Nếu bạn chưa xác minh số điện thoại hoặc chưa có tài khoản trên
+            + Nếu bạn chưa xác minh số điện thoại hoặc chưa có tài khoản trên
             POPS, vui lòng làm theo hướng dẫn sử dụng bên dưới để đăng ký gói
             cước thành công.
           </li>
           <li>
-            (**) Sau 12h kể từ khi đăng ký thành công, bạn có thể xem lại thông
-            tin gói cước đã đăng ký trong phần Tài khoản.
+            + Sau 12h kể từ khi đăng ký thành công, bạn có thể xem lại thông tin
+            gói cước đã đăng ký trong phần Tài khoản.
           </li>
-          <li>(***) 1 chu kỳ tương đương 30 ngày.</li>
+          <li>+ 1 chu kỳ tương đương 30 ngày.</li>
         </ul>
         <h1 className="text-34 font-medium">Miễn phí dung lượng 3G/4G</h1>
 
